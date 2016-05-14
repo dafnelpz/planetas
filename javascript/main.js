b0ab1c3a14a7611a6c15856a3f0e1aade01eda7e
dafnelpz <iktan.da@gmail.com>
2016-05-13 22:00:58 -0500

    efecto del sol

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,4 @@
 $(".planeta1").addClass("animated flip infinite");
 $(".planeta2").addClass("animated flip infinite");
 $(".planeta3").addClass("animated flip infinite");
+$(".sol").addClass("animated rotateIn infinite");
